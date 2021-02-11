36801053c2eb91a10147624a94e013abac21dd95
Gregory Vincic <g@7de.se>
2021-02-11 06:36:39 +0100

    Refactor Report to hold it's own entries

--- editor.go
+++ editor.go
@@ -7,8 +7,6 @@ import (
 	"io"
 	"os"
 	"regexp"
-
-	"github.com/gregoryv/nexus"
 )
 
 // https://github.com/OWASP/IoT-Security-Verification-Standard-ISVS
@@ -130,72 +128,9 @@ func (me *Editor) SaveReport(filename string, report Report) error {
 
 // WriteReport writes a markdown report
 func (me *Editor) WriteReport(w io.Writer, report Report) error {
-	p, err := nexus.NewPrinter(w)
-	p.Println("#", report.Title)
-
-	p.Println("## Summary")
-	p.Println()
-	p.Println("- L1:", me.Stats(me.list(1)))
-	p.Println("- L2:", me.Stats(me.list(2)))
-	p.Println("- L3:", me.Stats(me.list(3)))
-
-	p.Println()
-	p.Println("## Applicable")
-	for _, e := range me.entries {
-		if !e.Applicable {
-			continue
-		}
-		p.Printf("- %s **%s** %s\n", e.checkbox(), e.ID, e.Description)
-	}
-
-	p.Println()
-	p.Println("## Not Applicable")
-	for _, e := range me.entries {
-		if e.Applicable {
-			continue
-		}
-		desc := e.Description
-		if report.ShortDescriptionNA {
-			desc = e.shortDesc()
-		}
-		p.Printf("- %s %s\n", e.ID, desc)
-	}
-
-	return *err
-}
-
-func (me *Editor) Stats(entries []Entry) string {
-	var num int
-	var verified int
-	var applicable int
-	for _, e := range entries {
-		num++
-		if e.Applicable {
-			applicable++
-		}
-		if e.Verified {
-			verified++
-		}
-	}
-	return fmt.Sprintf("%d/%d applicable (total %d)", verified, applicable, num)
-}
-
-func (me *Editor) list(level int) []Entry {
-	res := make([]Entry, 0)
-	if level < 1 || level > 3 {
-		panic(fmt.Errorf("no such level %v", level))
-	}
-	for _, e := range me.entries {
-		switch {
-		case level == 1 && e.L1:
-			res = append(res, e)
-		case level == 2 && e.L2:
-			res = append(res, e)
-		case level == 3 && e.L3:
-			res = append(res, e)
-		}
-	}
-	return res
+	report.entries = me.entries
+	_, err := report.WriteTo(w)
+	return err
 }
 
 type Entry struct {
@@ -229,18 +164,4 @@ func (me *Entry) shortDesc() string {
 		return me.Description
 	}
 	return me.Description[:80] + "..."
-}
-
-// ----------------------------------------
-
-func NewReport(title string) *Report {
-	return &Report{
-		Title:              title,
-		ShortDescriptionNA: true,
-	}
-}
-
-type Report struct {
-	Title              string
-	ShortDescriptionNA bool // true to shorten description for all non applicable
 }
---
+++ report.go
@@ -0,0 +1,91 @@
+package owasp
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/gregoryv/nexus"
+)
+
+func NewReport(title string) *Report {
+	return &Report{
+		Title:              title,
+		ShortDescriptionNA: true,
+	}
+}
+
+type Report struct {
+	entries            []Entry
+	Title              string
+	ShortDescriptionNA bool // true to shorten description for all non applicable
+}
+
+// WriteReport writes a markdown report
+func (me *Report) WriteTo(w io.Writer) (int64, error) {
+	p, err := nexus.NewPrinter(w)
+	p.Println("#", me.Title)
+
+	p.Println("## Summary")
+	p.Println()
+	p.Println("- L1:", me.Stats(me.list(1)))
+	p.Println("- L2:", me.Stats(me.list(2)))
+	p.Println("- L3:", me.Stats(me.list(3)))
+
+	p.Println()
+	p.Println("## Applicable")
+	for _, e := range me.entries {
+		if !e.Applicable {
+			continue
+		}
+		p.Printf("- %s **%s** %s\n", e.checkbox(), e.ID, e.Description)
+	}
+
+	p.Println()
+	p.Println("## Not Applicable")
+	for _, e := range me.entries {
+		if e.Applicable {
+			continue
+		}
+		desc := e.Description
+		if me.ShortDescriptionNA {
+			desc = e.shortDesc()
+		}
+		p.Printf("- %s %s\n", e.ID, desc)
+	}
+
+	return p.Written, *err
+}
+
+func (me *Report) Stats(entries []Entry) string {
+	var num int
+	var verified int
+	var applicable int
+	for _, e := range entries {
+		num++
+		if e.Applicable {
+			applicable++
+		}
+		if e.Verified {
+			verified++
+		}
+	}
+	return fmt.Sprintf("%d/%d applicable (total %d)", verified, applicable, num)
+}
+
+func (me *Report) list(level int) []Entry {
+	res := make([]Entry, 0)
+	if level < 1 || level > 3 {
+		panic(fmt.Errorf("no such level %v", level))
+	}
+	for _, e := range me.entries {
+		switch {
+		case level == 1 && e.L1:
+			res = append(res, e)
+		case level == 2 && e.L2:
+			res = append(res, e)
+		case level == 3 && e.L3:
+			res = append(res, e)
+		}
+	}
+	return res
+}
