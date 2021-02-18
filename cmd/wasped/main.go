7290285b7ad6c6b6e227afc2fea14f10c36b0e04
Gregory Vincic <g@7de.se>
2021-02-18 11:44:07 +0100

    Add flag --manual to wasped

--- cmd/wasped/main.go
+++ cmd/wasped/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gregoryv/cmdline"
 	"github.com/gregoryv/owasp"
@@ -14,6 +15,10 @@ func main() {
 		help     = cli.Flag("-h, --help")
 		verify   = cli.Option("--verify", "ID to set as verified").String("")
 		unverify = cli.Option("--unverify", "ID to set as unverified").String("")
+		manstr   = cli.Option("-m, --manual",
+			"Comma separated list of manual verification notes.",
+			"Format: YYYY-MM-DD, By, How",
+		).String("")
 
 		rfile   = cli.Option("-r, --report", "Markdown file to save report to").String("")
 		title   = cli.Option("-t, --title", "Title of the report").String("Report")
@@ -34,18 +39,41 @@ func main() {
 		log.Fatal(cli.Error())
 
 	case file == "":
-		log.Fatal("empty file, see -help")
+		log.Fatal("empty file, see --help")
 	}
 
 	ed := owasp.NewEditor()
 	ed.Load(file)
 
-	if verify != "" {
-		must(ed.SetVerified(verify, true))
+	var man *owasp.Manual
+	if manstr != "" {
+		parts := strings.Split(manstr, ",")
+		if len(parts) != 3 {
+			log.Fatal("invalid --manual text, see --help")
+		}
+		man = &owasp.Manual{
+			How:  parts[2],
+			When: parts[1],
+			By:   parts[0],
+		}
 	}
+	var (
+		id       string = verify
+		verified bool   = true
+	)
 	if unverify != "" {
-		must(ed.SetVerified(unverify, false))
+		id = unverify
+		verified = false
 	}
+
+	switch {
+	case man != nil:
+		must(ed.SetManuallyVerified(id, verified, *man))
+
+	default:
+		must(ed.SetVerified(id, verified))
+	}
+
 	if rfile != "" {
 		report := ed.NewReport(title)
 		report.ShortDescriptionNA = shortna
